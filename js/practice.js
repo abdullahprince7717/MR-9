ccb89857f2f627c79aadbb210ac5b0819633a39a
abdullahprince7717 <abdullahprince7717@gmail.com>
2023-11-13 11:27:59 +0500

    ternary op, switch statement and for loop

--- js/practice.js
+++ js/practice.js
@@ -52,3 +52,55 @@ console.log(a)
 
 a = 6 + 7 * (8 / 9)
 console.log(a)
+
+
+console.log("=====================================")
+
+// Ternary Operator
+
+let age = 18
+let result = (age > 18) ? "Adult" : "Child"
+console.log(result)
+
+console.log("=====================================")
+
+// Switch Case
+
+let day = 1
+switch (day) {
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("Invalid")
+}
+
+console.log("=====================================")
+
+// For Loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log("=====================================")
+
+// While Loop
